ba8e830f98d698dcef6187a3e9791b2e2aad81d0
ahmed_yonis <ahmed.younis_6@hotmail.com>
2026-02-15 22:52:32 +0000

    get unit paid amount

--- frontline_elsalem/public/js/payment_entry.js
+++ frontline_elsalem/public/js/payment_entry.js
@@ -33,12 +33,26 @@ frappe.ui.form.on('Payment Entry', {
     },
 
 	unit: function(frm) {
+		frm.events.get_unit_details(frm);
+	},
+
+	unit_payment_type: function(frm) {
+		frm.events.get_unit_details(frm);
+	},
+
+	get_unit_details: function(frm) {
 		if (frm.doc.unit) {
 			// get customer name from unit
-			frappe.db.get_value("unit", frm.doc.unit, "customer_link", function(doc) {
-				frm.set_value({
-					party: doc.customer_link,
-					party_type: "Customer"
+			frappe.db.get_doc("unit", frm.doc.unit)
+			.then(doc => {
+				frm.set_value("party", doc.customer_link);
+				frm.set_value("party_type", "Customer");
+				$.each(doc.contract_details, (index, row) => {
+					if (frm.doc.unit_payment_type == row.paymenttype) {
+						let remaining_amount = row.installments - row.paid;
+						frm.set_value("paid_amount", remaining_amount);
+						return;
+					}
 				});
 			});
 		}
